1b696d1f74b4c5e21a056f77b4a7d5288b0e03af
ContaMeia <dev.contaemeia@gmail.com>
2024-07-25 10:46:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import './global.css'; // Import global styles
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
